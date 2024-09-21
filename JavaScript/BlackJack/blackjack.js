b7aabda799ddef36623117a2ecb37889894bc018
Daniel Fontalva Ostio <145746911+Danielfo684@users.noreply.github.com>
2024-09-21 17:34:00 +0200

    7 y medio

--- JavaScript/BlackJack/blackjack.js
+++ JavaScript/BlackJack/blackjack.js
@@ -1,16 +1,46 @@
+console.log("Let's play 7 and a half")
+for (let i = 0; i < 5; i++) {
+    console.log(i + 1 + "round!");
+    myCards = 0;
+    oppCards = 0;
+    oppCards = checkCard(randomCard());
+    keepPlaying = 'Y';
 
-class card {
-    constructor(name, numero) {
-        this.name = name;
-        this.numero = numero;
+    //Opponents turn
+    while (oppCards < 5) {
+        oppCards = oppCards + checkCard(randomCard());
+    }
+    while (keepPlaying != 'N') {
+        myCards = myCards + checkCard(randomCard());
+        if (myCards > 7.5) {
+            console.log("Your cards add up to " + myCards + " so you lost.");
+            keepPlaying = "N";
+        } else {
+            keepPlaying = prompt("Your cards add up to " + myCards + " Do you want another card? Type Y for Yes or N for No.");
+            if (keepPlaying != "Y") {
+                console.log("Your cards add up to " + myCards);
+                console.log("Your rival cards add up to " + oppCards);
+                if (oppCards > myCards && oppCards < 8) {
+                    console.log("You lost.");
+                } else {
+                    console.log("You won.");
+                }
+            }
+        }
     }
 }
 
-const deck = [];
-for (i = 0; i < 13; i++) {
-    numero = i;
-    let card = numero,numero;
-deck[numero] = card;
+
+
+
+
+function randomCard() {
+    return Math.ceil(Math.random() * 10);
 }
 
-console.log(deck[Math.random(0,12)]);
+function checkCard(a) {
+    if (a >= 8) {
+        a = 0.5;
+    }
+    return a;
+}
