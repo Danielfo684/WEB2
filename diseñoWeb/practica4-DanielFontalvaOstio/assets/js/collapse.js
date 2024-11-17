33b07d348e011aca336af746d95be40b5e28cd7d
Daniel Fontalva Ostio <145746911+Danielfo684@users.noreply.github.com>
2024-11-17 23:32:41 +0100

    práctica4

--- diseñoWeb/practica4-DanielFontalvaOstio/assets/js/collapse.js
+++ diseñoWeb/practica4-DanielFontalvaOstio/assets/js/collapse.js
@@ -1,5 +1,6 @@
 const navBar = document.querySelector('.nav-bar');
 const menuOn = document.querySelector('.menu-on');
+const nav = document.querySelector('.nav-bar');
 if (navBar) {
     navBar.addEventListener('click', () => {
         navBar.classList.toggle('open');
