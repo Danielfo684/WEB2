fc720bd1d0988468b65ae7444868dd4a743995a0
Daniel Fontalva Ostio <145746911+Danielfo684@users.noreply.github.com>
2024-12-29 15:52:55 +0100

    a

--- diseñoWeb/practica5-DanielFontalvaOstio/assets/js/fakeParalax.js
+++
@@ -1,6 +0,0 @@
-let text = document.getElementById('main-tag');
-window.addEventListener('scroll', function() {
-    let value = window.scrollY;
-    console.log(value);
-    text.style.marginTop = value * 1 + 'px';
-});
