a1dd804c0586a861f901030f47539daaef2e893a
Daniel Fontalva Ostio <145746911+Danielfo684@users.noreply.github.com>
2025-01-15 17:43:58 +0100

    a

--- diseñoWeb/practica5-DanielFontalvaOstio/assets/js/collapse.js
+++
@@ -1,9 +0,0 @@
-const navBar = document.querySelector('.nav-bar');
-const menuOn = document.querySelector('.menu-on');
-const nav = document.querySelector('.nav-bar');
-if (navBar) {
-    navBar.addEventListener('click', () => {
-        navBar.classList.toggle('open');
-        menuOn.classList.toggle('menu-mobile');
-    });
-};
